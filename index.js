cab32888d1df50b61768d29eae0c338c42bfe87c
Bishal Bhandari <contact@bishalbhandari.com>
2022-09-23 20:35:26 +0100

    added grade calculator

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 Projects = [
     {
-        name: "Password Generator",
-        img: "../data/images/password-generator.png",
-        desc:"My first javascript project and i am currently using this to create safe password for my online accounts. In future i am planning on making this more user friendly by letting them choose the characters that they want in password.",
-        demoUrl:"https://projects.bishalbhandari.com/password-generator/",
-        sourceCode:"https://github.com/itsbishalb/Password-Generator"
+        name: "University Grade Calculator",
+        img: "../data/images/grade-calculator.png",
+        desc:"Created with React.js, this is my first react project. I wanted to make this calculator so i cancalculate my semester and year average grade. The calculator works as expected but it haven't got all the features i wanted yet and will be working on creating those features and possibly have own backend as well to store past grade.",
+        demoUrl:"https://projects.bishalbhandari.com/university-grade-calculator/",
+        sourceCode:"https://github.com/itsbishalb/grade-calculator"
     },
     {
         name: "Password Generator",
@@ -21,8 +21,6 @@ Projects = [
         sourceCode:"https://github.com/itsbishalb/Password-Generator"
     }
 ]
-
-
 
 function loadHtml () {
         return Projects.map((project) => {
